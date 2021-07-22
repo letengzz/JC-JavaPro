61407a626e20852d17b2bb346ba86044ee5db104
letengzz <2020885569@qq.com>
2021-07-22 14:57:29 +0800

    UPDATE

--- JavaSE/src/基本语法/基础/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package 基本语法.基础;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- JavaSE/src/基本语法/流程控制/顺序结构/顺序结构.java
+++
@@ -1,7 +0,0 @@
-package 基本语法.流程控制.顺序结构;
-
-public class 顺序结构 {
-    public static void main(String[] args) {
-        //程序从上到下执行
-    }
-}
